17d440408d1fd5ae79f16f27e2bf99dac22d5ce1
Jeffrey Ben <54586510+jefficy1234@users.noreply.github.com>
2020-03-27 12:27:11 -0400

    POKEMON API HOOKS PAIRING

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useRef } from "react";
+import { PokemonType } from "./PokemonType";
+import { usePokemonAPI } from "./hooks";
 
 export function Pokedex() {
+  const [selectedPokemon, getPokemonInfo] = usePokemonAPI();
+
+  const searchBox = useRef(null);
+
   return (
     <div>
-      <h4>
-        Edit <code>src/Pokedex.js</code> and save to reload.
-      </h4>
+      {selectedPokemon && (
+        <div>
+          {selectedPokemon.name}
+          <PokemonType type={selectedPokemon.types[0].type.name} />
+          {selectedPokemon.types.length > 1 && (
+            <PokemonType type={selectedPokemon.types[1].type.name} />
+          )}
+
+          <img src={selectedPokemon.sprites.front_default} alt="sprite" />
+        </div>
+      )}
+      <p>Select Pokemon:</p>
+      <button onClick={() => getPokemonInfo("Squirtle")}>Squirtle</button>
+      <button onClick={() => getPokemonInfo("Bulbasaur")}>Bulbasaur</button>
+      <button onClick={() => getPokemonInfo("Charmander")}>Charmander</button>
+      <input ref={searchBox} />
+      <button onClick={() => getPokemonInfo(searchBox.current.value)}>
+        Search
+      </button>
     </div>
   );
 }
